d1b50ae4edb97ee079b3f94b52e8a917662c175c
Olivia Brown <obrown917@gmail.com>
2016-07-12 11:39:17 -0700

    Simple Instagram replica written in Swift for iOS.

---
+++ Makestagram/ViewControllers/PhotoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoViewController.swift
+//  Makestagram
+//
+//  Created by Olivia on 6/22/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+  /* This ViewController is not actually used, it's a dummy that exists to enable
+   taking photos upon tab bar button press */
+
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)!
+
+    // the following line ensures that the camera icon is rendered white instead of the typical gray of a deactivated tab bar item
+    self.tabBarItem.image = UIImage(named: "camera")?.imageWithRenderingMode(.AlwaysOriginal)
+  }
+}
